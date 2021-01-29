90dda7b15b4c0e951b12224f6fcf488721c0e491
unknown <baileke@ucmail.uc.edu>
2021-01-28 19:43:44 -0500

    adding powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,11 @@
+﻿
+#$hello ="Hello, Powershell!"
+#write-host($Hello)
+function getIP {
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+write-host(getIP)
+$IP = getIP
+Write-Host("This machines IP is $IP")
+Write-Host("This machine's IP is {0}" -f $IP)
